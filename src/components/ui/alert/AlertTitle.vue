727f6d29d4ad907341600f08e818c180fd599220
BackRunner <dev@backrunner.top>
2024-08-25 22:10:57 +0800

    Squashed commit of the following:
    commit 0842c1708be8d805cec6d0c0f75a2fe7d1a0895f
    Author: BackRunner <dev@backrunner.top>
    Date:   Sun Aug 25 21:57:51 2024 +0800
    
        fix: do not send heartbeat to closed controller
    
    commit 84ce79d3d3cdd2e213425f96edf546708bf1abd4
    Author: BackRunner <dev@backrunner.top>
    Date:   Sun Aug 25 21:56:18 2024 +0800
    
        fix: cannot remove expired subscription
    
    commit 4db710a35445983089f10286f7dfa45e03fe666b
    Author: BackRunner <dev@backrunner.top>
    Date:   Sun Aug 25 20:35:54 2024 +0800
    
        feat: enhance the experience of the alert and the guidance
    
    commit 4cfc1ddb52c48d593b962692ec90814d24c614ba
    Author: BackRunner <dev@backrunner.top>
    Date:   Sun Aug 25 20:26:00 2024 +0800
    
        feat: add privacy policy page
    
    commit 99201569027302ae3b4bbf04f632f74f2d44e6a3
    Merge: e937f8c 19e8a5b
    Author: BackRunner <dev@backrunner.top>
    Date:   Sun Aug 25 18:50:36 2024 +0800
    
        Merge branch 'main' into dev
    
    commit e937f8c2d13180bd0fe599afd1f6e5385f0742a0
    Author: BackRunner <dev@backrunner.top>
    Date:   Sun Aug 25 18:48:09 2024 +0800
    
        chore: update roadmap
    
    commit 7a7cd9f60c887d02e2a352408db73a8fc0756410
    Author: BackRunner <dev@backrunner.top>
    Date:   Sun Aug 25 18:45:16 2024 +0800
    
        feat: allow to set an allowed email list
    
    commit eeb575b933d6347e54ba7ebded3a201b3895a50c
    Author: BackRunner <dev@backrunner.top>
    Date:   Sun Aug 25 18:27:09 2024 +0800
    
        chore: update roadmap
    
    commit 25819111bf8b832d567fc4220187022d42db61f2
    Author: BackRunner <dev@backrunner.top>
    Date:   Sun Aug 25 18:26:33 2024 +0800
    
        chore: update roadmap
    
    commit a64c90169d16e690b1a0943f893e83e5e785c3ba
    Author: BackRunner <dev@backrunner.top>
    Date:   Sun Aug 25 18:22:38 2024 +0800
    
        fix: align the encoding method
    
    commit e639829e4596d44d875342a1ab57a32932245198
    Author: BackRunner <dev@backrunner.top>
    Date:   Sun Aug 25 18:19:06 2024 +0800
    
        feat: allow to push notification with custom icon & add highlight effect to the card
    
    chore: bump version

--- src/components/ui/alert/AlertTitle.vue
+++ src/components/ui/alert/AlertTitle.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { inject } from 'vue';
-import type { ComputedRef, HTMLAttributes } from 'vue';
+import { inject, ref } from 'vue';
+import type { ComputedRef, HTMLAttributes, Ref } from 'vue';
 import { Icon } from '@iconify/vue';
 import { cn } from '@/lib/utils';
 
@@ -10,6 +10,10 @@ const props = defineProps<{
 
 const closable = inject<ComputedRef<boolean>>('closable');
 const closeAlert = inject('closeAlert', () => {});
+const showDismissForever = inject<Ref<boolean>>('showDismissForever');
+const dismissForever = inject('dismissForever', () => {});
+const dismissTemporarily = inject('dismissTemporarily', () => {});
+const allowDismissForever = inject<ComputedRef<boolean>>('allowDismissForever');
 </script>
 
 <template>
@@ -17,8 +21,17 @@ const closeAlert = inject('closeAlert', () => {});
     <h5 :class="cn(`${closable ? '' : 'mb-1 '}font-medium leading-none tracking-tight`)">
       <slot />
     </h5>
-    <button v-if="closable" @click="closeAlert">
-      <Icon icon="mdi:close" class="w-4 h-4" />
-    </button>
+    <div v-if="closable" class="flex items-center">
+      <template v-if="!showDismissForever || !allowDismissForever">
+        <button @click="closeAlert">
+          <Icon icon="mdi:close" class="w-4 h-4" />
+        </button>
+      </template>
+      <template v-else-if="allowDismissForever">
+        <span class="mr-2 text-sm font-semibold">Dismiss forever?</span>
+        <button @click="dismissForever" class="mr-2 text-sm font-medium">Yes</button>
+        <button @click="dismissTemporarily" class="text-sm font-medium">No</button>
+      </template>
+    </div>
   </div>
 </template>
